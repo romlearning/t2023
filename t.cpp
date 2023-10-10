2ed3764d7e8adc4b36fd0270363e193a465e0b8b
Roman <ROliynykov@gmail.com>
2023-10-10 14:23:43 +0300

    A new functionality added to test

--- t.cpp
+++ t.cpp
@@ -8,6 +8,7 @@ int main()
 	cout << "The second printed line" << endl;
 	cout << "A new functionality in the test branch" << endl;
 	cout << "The third line in the main branch" << endl;
+	cout << "A new functionality added to the test branch" << endl;
 	return 0;
 }
 
